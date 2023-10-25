4b4c23c1c38b92872b9b9d73fcf948dfcb969031
Aristide Fumo <aristide.fumo@sopht.com@sopht.com>
2023-10-25 09:11:24 +0200

    last code

--- src/main/java/org/example/EmailSend.java
+++ src/main/java/org/example/EmailSend.java
@@ -1,41 +1,47 @@
 package org.example;
 
+import java.util.Objects;
+
 final class EmailSend {
 
     private static EmailSend emailSend;
 
-    public static EmailSend of() {
+    private final String domain;
+
+    public static EmailSend of(String domain) {
         if (emailSend == null) {
-            return new EmailSend();
+            emailSend = new EmailSend(domain);
         }
         return emailSend;
     }
 
-    private EmailSend() {
+    public String getDomain() {
+        return domain;
+    }
+
+    private EmailSend(String domain) {
+        this.domain = domain;
     }
 
     public void sendEmail(String email) {
-        System.out.println("Email sent to " + email);
+        System.out.println("Email sent to " + email + " from " + domain);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmailSend emailSend = (EmailSend) o;
+        return Objects.equals(domain, emailSend.domain);
     }
 
     @Override
     public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+        return Objects.hash(domain);
     }
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,37 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        EmailSend emailSend = EmailSend.of();
-        User user = UserBuilder
+
+        EmailSend emailSend = EmailSend.of("example.com");
+        User userJohn = UserBuilder
                 .of(emailSend)
                 .setLastName("Doe")
                 .setFirstName("John")
                 .setEmail("john.doe@address.com")
                 .setAddress("address")
                 .build();
+        System.out.println(userJohn);
 
 
-        System.out.println(user);
+        System.out.println("\n----- 2nd user -----\n");
+
+        EmailSend emailSend2 = EmailSend.of("error.com");
+        User userJane = UserBuilder
+                .of(emailSend2)
+                .setLastName("Doe2")
+                .setFirstName("Jane")
+                .setEmail("jane.doe@address2.com")
+                .setAddress("address2")
+                .build();
+
+
+        System.out.println(userJane);
+
+
+        System.out.println("\n----- Verification Singleton -----\n");
+
+
+        System.out.println("EmailSend instances are equal: " + emailSend.equals(emailSend2));
+        System.out.println("But EmailSend domains are equal: " + emailSend2.getDomain() + " == " + emailSend.getDomain());
     }
 }
